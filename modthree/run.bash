98bebd9b544c8844b267165e74da152d87720d0e
320834 <justinchen1000@gmail.com>
2020-09-19 13:51:49 -0400

    add run bash script

---
+++ modthree/run.bash
@@ -0,0 +1,11 @@
+hdfs dfs -mkdir bigdata/modthree
+
+hdfs dfs -put data.txt bigdata/modthree
+
+javac -classpath `yarn classpath` -d . WordCountMapper.java
+javac -classpath `yarn classpath` -d . WordCountReducer.java
+javac -classpath `yarn classpath`:. -d . MaxTemperature.java
+
+jar -cvf wordCount.jar *.class
+
+hadoop jar wordCount.jar WordCount bigdata/modthree/data.txt bigdata/modthree/output
