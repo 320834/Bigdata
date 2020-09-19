e915204ac941d512fdd11936ca3dc3c4b54ad93a
320834 <justinchen1000@gmail.com>
2020-09-19 14:00:44 -0400

    fix syntax errors

--- modthree/WordCountReducer.java
+++ modthree/WordCountReducer.java
@@ -3,7 +3,9 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class WordCountReducer{
+public class WordCountReducer 
+extends Reducer<Text, IntWritable, Text, IntWritable> {
+
     @Override
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 
