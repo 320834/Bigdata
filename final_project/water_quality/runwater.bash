432d8e1ba8efaee19fe290adbe3dbe6d59202659
panda <justinchen1000@gmail.com>
2020-11-20 13:13:13 -0500

    some

--- final_project/water_quality/runwater.bash
+++ final_project/water_quality/runwater.bash
@@ -4,4 +4,4 @@ hdfs dfs -mkdir bigdata/final_project/water_quality/clean
 hdfs dfs -mkdir bigdata/final_project/water_quality/analyze
 hdfs dfs -mkdir bigdata/final_project/water_quality/raw_data
 
-bash ./clean/runwaterclean.bash
+# bash ./clean/runwaterclean.bash
