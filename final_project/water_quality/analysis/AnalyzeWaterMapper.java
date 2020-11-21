f386afb320e42e5edf0b6c6214d3be8e8d7b7fe0
panda <justinchen1000@gmail.com>
2020-11-20 21:50:30 -0500

    change some stuff

--- final_project/water_quality/analysis/AnalyzeWaterMapper.java
+++ final_project/water_quality/analysis/AnalyzeWaterMapper.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 
 public class AnalyzeWaterMapper extends Mapper<LongWritable, Text, Text, AnalyzeWritable> 
 {
