3764a7635ca23729b422262fa3ef1440ec9027d1
panda <justinchen1000@gmail.com>
2020-11-20 21:09:29 -0500

    change analyze bash file

--- final_project/water_quality/analysis/run_water_analyze.bash
+++ final_project/water_quality/analysis/run_water_analyze.bash
@@ -8,8 +8,8 @@ javac -classpath `yarn classpath`:. -d . AnalyzeWater.java
 
 jar -cvf AnalyzeWater.jar AnalyzeWaterMapper.class AnalyzeWaterReducer.class AnalyzeWater.class AnalyzeWaterWritable.class
 
-#Run Jar
-hadoop jar AnalyzeWater.jar AnalyzeWater bigdata/final_project/inputdata.csv bigdata/final_project/output_analysis
+#Run Jar from input file from cleaned
+hadoop jar AnalyzeWater.jar AnalyzeWater bigdata/final_project/output/part-r-00000 bigdata/final_project/output_analysis
 
 #Move file from hdfs to local
 hdfs dfs -copyToLocal bigdata/final_project/output_analysis/part-r-00000 /home/jc8017/Repository/Bigdata/final_project/water_quality/analysis
