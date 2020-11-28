01eba8967add301b5fa67167ad8de97c689a30ee
panda <justinchen1000@gmail.com>
2020-11-28 16:19:23 -0500

    change profiling bash commands

--- final_project/water_quality/profiling_code/run_water_profile.bash
+++ final_project/water_quality/profiling_code/run_water_profile.bash
@@ -9,3 +9,9 @@ jar -cvf CountCounty.jar CountCountyMapper.class CountCountyReducer.class CountC
 
 #Run Jar from input file from cleaned
 hadoop jar CountCounty.jar CountCounty /user/ma4759/infrastructure/water_quality/analysis_output/part-r-00000 /user/ma4759/infrastructure/water_quality/profiling_output
+
+# Move to local directory
+hdfs dfs -copyToLocal /user/ma4759/infrastructure/water_quality/profiling_output/part-r-00000 ./
+
+#Rename copied filed
+mv ./part-r-00000 ./profile_data
--- final_project/water_quality/run_water_quality.bash
+++ final_project/water_quality/run_water_quality.bash
@@ -15,9 +15,9 @@ bash run_water_analyze.bash
 cd ..
 
 # Do profiling code for water quality
-# cd profiling_code
-# bash run_water_profile.bash
-# cd ..
+cd profiling_code
+bash run_water_profile.bash
+cd ..
 
 # Take water analysis final and put it into combine input
 mv ./ana_code/analyze_data ../combine/final_input
